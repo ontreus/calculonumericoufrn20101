e65f57ee96d44fe7233aca7ff397e9a91427ba42
vit.alcantara <vit.alcantara@c1f52606-3033-11df-9ac8-ff20597b2c9b>
2010-03-19 13:13:05 +0000

    Implementados Fatoração LU, substituição progressiva, substituição regressiva, troca linha e Fat LU com pivotamento parcial.

--- CalculoNumericoInJava/src/CalculoNumerico.java
+++ CalculoNumericoInJava/src/CalculoNumerico.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * 
  */
@@ -42,6 +44,237 @@ public class CalculoNumerico {
 		String rightSide = multiplicacao_sucessiva(decimal - (int)decimal);
 		return leftSide + "," + rightSide;
 	}
+	
+	public static double[][] multiplicarMatrizes(double[][] multiplicadora,double[][] multiplicando)
+	{
+		if(multiplicadora[0].length != multiplicando.length)
+		{
+			System.out.println("O tamanho das matrizes não são compatíveis");
+			return null;
+		}		
+				
+		double[][] resultado = new double[multiplicadora.length][multiplicando[0].length];
+		for(int i = 0 ; i < resultado.length ; i++)
+		{
+			for(int j = 0 ; j < resultado[0].length ; j++)
+			{
+				resultado[i][j] = 0;
+			}
+		}		
+		
+		for(int i = 0 ; i < multiplicadora.length ; i++)
+		{
+			for(int j = 0 ; j < multiplicando[0].length ; j++)
+			{
+				for(int k = 0 ; k < multiplicando.length ; k++)
+				{
+					resultado[i][j] += multiplicadora[i][k] * multiplicando[k][j];
+				}
+			}
+		}
+		return resultado;
+		
+	}
+	
+	public static void printMatriz(double[][] matriz)
+	{
+		for(int i = 0 ; i < matriz.length ; i++)
+			{
+				for(int k = 0 ; k < matriz[0].length ; k++)
+				{
+					System.out.print(matriz[i][k]+ ",");
+				}
+				System.out.println();
+					
+			}
+	}
+	
+	public static HashMap<double[][], double[][]> obterMatrizLInversaE_UdaFatoracaoLU(double[][] matriz,double[][] b)
+	{
+		double[][] U = matriz;
+		double[][] temp = new double[0][0];
+		for(int i = 0 ; i < matriz.length - 1 ; i++)
+		{
+			temp = transformadaDeGauss(matriz, i);
+			b = multiplicarMatrizes(temp, b);
+			matriz = multiplicarMatrizes(temp, matriz);
+			temp = matriz;
+		}
+		System.out.println("Imprimindo matriz U:");
+		printMatriz(temp);
+		System.out.println("Imprimindo matriz L inversa");
+		printMatriz(b);
+		
+		HashMap<double[][], double[][]> hmap = new HashMap<double[][], double[][]>();
+		hmap.put(temp, b);
+		return hmap;
+	}
+	
+	
+	public static double[][] setIdentidadeToMatriz(double[][] matriz)
+	{
+		for(int index = 0 ; index < matriz.length ; index++)
+		{
+			for(int j = 0; j < matriz[0].length ; j++ )
+			{
+				if(j == index)
+				{
+					matriz[index][j] = 1;
+				}
+				else
+				{
+					matriz[index][j] = 0;
+				}
+			}
+		}
+		return matriz;
+	}
+	
+	/**
+	 * Algoritmo da folha do Professor.
+	 */
+	public static void fatoracaoLU(double[][] a,double[][] b,boolean comPivotamentoParcial)
+	{
+		double[][] u = a;
+		double[][] linv = new double[a.length][a[0].length];
+		setIdentidadeToMatriz(linv);
+		double[][] m = new double[a.length][a[0].length];
+		for(int i = 0 ; i < a.length -1 ; i++)
+		{
+			if(comPivotamentoParcial)
+			{
+				troca_linha(u);
+			}			
+			setIdentidadeToMatriz(m);
+			for(int j = i + 1 ; j < a.length ; j++)
+			{
+				m[j][i] = -u[j][i] / u[i][i];
+			}
+			linv = multiplicarMatrizes(m, linv);
+			u = multiplicarMatrizes(m, u);
+			b = multiplicarMatrizes(m, b);		
+		}
+		
+		double[][] x = new double[a.length][1];
+		
+		for(int i = a.length -1 ; i>=0 ; i--)
+		{
+			x[i][0] = b[i][0];
+			for(int j = i + 1; j < a.length ; j++)
+			{
+				x[i][0] = x[i][0] - u[i][j] * x[j][0];
+			}
+			x[i][0] = x[i][0] / u[i][i];
+		}
+		
+		System.out.println("Imprimindo linv:");
+		printMatriz(linv);
+		System.out.println("Imprimindo U");
+		printMatriz(u);
+		System.out.println("Imprimindo b");
+		printMatriz(b);
+		System.out.println("Imprimindo x");
+		printMatriz(x);
+		
+	}
+	
+	/**
+	 * Algoritmo do Professor
+	 * @param x
+	 * @param y
+	 * @param u
+	 */
+	public static void substituicao_regressiva(double[][] y,double[][] u)
+	{
+		double[][] x = new double[y.length][y[0].length];
+		for(int i = x.length - 1; i >= 0 ; i--)
+		{
+			x[i][0] = y[i][0];
+			for(int j = i+1; j < x.length ; j++ )
+			{
+				x[i][0] = x[i][0] - u[i][j] * x[i][0]; 
+			}
+			x[i][0] = x[i][0] / u[i][i];
+		}
+		System.out.println("Imprimindo x:");
+		printMatriz(x);
+	}
+	/**
+	 * Algoritmo do Professor.
+	 * @param l
+	 * @param b
+	 */
+	public static void substituicao_progressiva(double[][] l, double[][] b)
+	{
+		double[][] y = new double[b.length][b[0].length];
+		for(int i = 0 ; i < y.length ; i++)
+		{
+			y[i][0] = b[i][0];
+			for(int j = 0 ; j < i ; j++)
+			{
+				y[i][0] = y[i][0] - l[i][j] * y[j][0];
+			}
+			y[i][0] = y[i][0] / l[i][i];
+		}
+	}
+	
+	/**
+	 * Algoritmo feito por mim, mas que é do trabalho.
+	 * @param matriz
+	 * @return
+	 */
+	public static double[][] troca_linha(double[][] matriz)
+	{
+		for(int i = 0 ; i < matriz[0].length;i++)
+		{
+			for(int j = i + 1 ; j < matriz.length ; j++)
+			{
+				if(matriz[j][i] > matriz[i][i])
+				{
+					double temp = matriz[i][i];
+					matriz[i][i] = matriz[j][i];
+					matriz[j][i] = temp;
+				}
+			}
+		}
+		printMatriz(matriz);
+		return matriz;
+	}
+	
+	
+	
+	
+	
+	public static double[][] transformadaDeGauss(double[][] matriz, int level)
+	{
+		if(level > matriz.length)
+		{
+			System.out.println("O Level está fora do range da matriz.");
+			return null;
+		}
+		
+		double[][] resultado = new double[matriz.length][matriz[0].length];
+		for(int i = 0 ; i < matriz.length ; i++)
+		{
+			for(int j = 0 ; j < matriz[0].length ; j++)
+			{
+				if(i == j)
+				{
+					resultado[i][j] = 1;
+				}
+				else
+				{
+					resultado[i][j] = 0;
+				}
+			}
+		}
+		for(int i = level + 1 ; i < matriz.length ; i++)
+		{
+			resultado[i][level] = - matriz[i][level] / matriz[level][level];
+		}
+		return resultado;		
+	}
+	
 	
 	public static String divisao_sucessiva(int decimal)
 	{
--- CalculoNumericoInJava/src/Teste.java
+++ CalculoNumericoInJava/src/Teste.java
@@ -15,57 +15,80 @@ public class Teste {
 		//String a = CalculoNumerico.divisao_sucessiva(13);
 		//String a = CalculoNumerico.multiplicacao_sucessiva(0.625);
 		//System.out.println(a);
-		int choose = 0;
-		while(choose != 7)
-		{
-			System.out.println("Por favor, escolha uma das funções abaixo de acordo com o número:");
-			System.out.println("1: Binário para Decimal");
-			System.out.println("2: Briot-Ruffine Fração");
-			System.out.println("3: Briot-Ruffine Inteiro");
-			System.out.println("4: Divisão Sucessiva");
-			System.out.println("5: Multiplicação Sucessiva");
-			System.out.println("6: Binário Inteiro para Decimal(Força Bruta");
-			System.out.println("7: Sair do Programa");
-
-			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			try
-			{
-				choose = Integer.parseInt(reader.readLine());
-				if(choose == 1)
-				{
-					System.out.println("Digite a representação em binária do número, pode ser fração também");
-					System.out.println("Resultado: " + CalculoNumerico.binaryToDecimal(reader.readLine().trim()));
-					reader.readLine();
-				}
-				else if(choose == 2)
-				{
-					System.out.println("Digite a a representação em binária da fração, sendo esta menor que 1");
-					System.out.println("Resultado: " + CalculoNumerico.briot_Ruffine_Fracao(reader.readLine().trim()));
-					reader.readLine();
-				}
-				else if(choose == 7)
-				{
-					System.out.println("Saindo do programa...");
-					return;
-				}
-			
-			}
-			catch(NumberFormatException e)
-			{
-				System.out.println("O que você digitou não é um número");
-			}
-			
-			
-			
-			catch(Exception e)
-			{
-				System.out.println("Voce nao digitou um número");
-				System.out.println("Saindo do Programa");
-				return;
-			}		
-
-		}
-				
+//		int choose = 0;
+//		while(choose != 7)
+//		{
+//			System.out.println("Por favor, escolha uma das funções abaixo de acordo com o número:");
+//			System.out.println("1: Binário para Decimal");
+//			System.out.println("2: Briot-Ruffine Fração");
+//			System.out.println("3: Briot-Ruffine Inteiro");
+//			System.out.println("4: Divisão Sucessiva");
+//			System.out.println("5: Multiplicação Sucessiva");
+//			System.out.println("6: Binário Inteiro para Decimal(Força Bruta");
+//			System.out.println("7: Sair do Programa");
+//			System.out.println("8: Multiplicar duas matrizes");
+//
+//			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//			try
+//			{
+//				choose = Integer.parseInt(reader.readLine());
+//				if(choose == 1)
+//				{
+//					System.out.println("Digite a representação em binária do número, pode ser fração também");
+//					System.out.println("Resultado: " + CalculoNumerico.binaryToDecimal(reader.readLine().trim()));
+//					reader.readLine();
+//				}
+//				else if(choose == 2)
+//				{
+//					System.out.println("Digite a a representação em binária da fração, sendo esta menor que 1");
+//					System.out.println("Resultado: " + CalculoNumerico.briot_Ruffine_Fracao(reader.readLine().trim()));
+//					reader.readLine();
+//				}
+//				else if(choose == 7)
+//				{
+//					System.out.println("Saindo do programa...");
+//					return;
+//				}
+//				
+//			
+//			}
+//			catch(NumberFormatException e)
+//			{
+//				System.out.println("O que você digitou não é um número");
+//			}
+//			
+//			
+//			
+//			catch(Exception e)
+//			{
+//				System.out.println("Voce nao digitou um número");
+//				System.out.println("Saindo do Programa");
+//				return;
+//			}		
+//
+//		}
+//		double[][] m1 = {{1,2},{3,4},{5,6}};
+//		double[][] m2 = {{1,2,3},{4,5,6}};
+//		
+//		double[][] m3 = CalculoNumerico.multiplicarMatrizes(m1, m2);		
+//		for(int i = 0 ; i < m3.length ; i++)
+//		{
+//			for(int k = 0 ; k < m3[0].length ; k++)
+//			{
+//				System.out.print(m3[i][k]+ ",");
+//			}
+//			System.out.println();
+//				
+//		}
+		//double[][] m1 = {{1,4,7},{2,5,8},{3,6,11}};
+//		double[][] m1 = {{1,4,7},{0,-3,-6},{0,-6,-10}};
+//		double [][] m2 = CalculoNumerico.transformadaDeGauss(m1, 1);
+//		CalculoNumerico.printMatriz(m2);
+		//double[][] m2 = {{12},{15},{20}};
+		//CalculoNumerico.obterMatrizLInversaE_UdaFatoracaoLU(m1, m2);
+		//CalculoNumerico.fatoracaoLU(m1, m2);
+		double[][] m3 = {{0.0030,30.0000},{1.0000,4.0000}};
+		CalculoNumerico.troca_linha(m3);
 	}
 
 }
